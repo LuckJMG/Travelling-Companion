f9e9859c36d6b5ac4105af86f9c730ce748818a0
=LuckJMG <=lucas.mosquera13@gmail.com>
2022-07-24 18:29:24 -0400

    feat: add input handler

--- Assets/TravellingCompanion/Scripts/InputHandler.cs
+++ Assets/TravellingCompanion/Scripts/InputHandler.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 _playerInput;
+    private bool _isRunning;
+    private bool _isJumping;
 
-    // Update is called once per frame
-    void Update()
+    public Vector2 PlayerInput { get => _playerInput; }
+    public bool IsRunning { get => _isRunning; }
+    public bool IsJumping { get => _isJumping; }
+
+    private void Update()
     {
-        
+        _playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _isRunning = Input.GetKey(KeyCode.LeftControl) == true ? true : false;
+        _isJumping = Input.GetKeyDown(KeyCode.Space) == true ? true : false;
     }
 }
